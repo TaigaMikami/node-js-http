ae995831ef4f9ef873a0034c2f41db41648d6155
TaigaMikami <mikamimikami60@gmail.com>
2017-09-30 05:27:40 +0000

    remove Profile add Procfile

--- index.js
+++ index.js
@@ -2,23 +2,18 @@
 const http = require('http');
 const jade = require('jade');
 const server = http.createServer((req, res) => {
-  // console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
   const now = new Date();
   console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  // res.write(req.headers['user-agent']);
+
   switch (req.method) {
     case 'GET':
-      // res.write('GET ' + req.url + '\n');
-      // const fs = require('fs');
-      // const rs = fs.createReadStream('./form.html');
-      // rs.pipe(res);
       if (req.url === '/enquetes/yaki-shabu') {
-        res.write(jade.renderFile('./form.jade',{
+        res.write(jade.renderFile('./form.jade', {
           path: req.url,
-          firstItem: '焼肉',
+          firstItem: '焼き肉',
           secondItem: 'しゃぶしゃぶ'
         }));
       } else if (req.url === '/enquetes/rice-bread') {
@@ -27,44 +22,38 @@ const server = http.createServer((req, res) => {
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
-      } else if (req.url === '/enquetes/sushi-pizza') {
-        res.write(jade.renderFile('./form.jade',{
+      } else if(req.url === '/enquetes/sushi-pizza') {
+        res.write(jade.renderFile('./form.jade', {
           path: req.url,
           firstItem: '寿司',
           secondItem: 'ピザ'
-        }));
+        }))
       }
       res.end();
       break;
     case 'POST':
-      res.write('POST ' + req.url + '\n');
       let body = [];
       req.on('data', (chunk) => {
         body.push(chunk);
       }).on('end', () => {
         body = Buffer.concat(body).toString();
-        // console.info('[' + now + '] Data posted:' + body);
         const decoded = decodeURIComponent(body);
-        console.info('[' + now + '] 投稿: ' + decoded );
-        res.write('' + decoded + 'が投稿されました');
+        console.info('[' + now + '] 投稿: ' + decoded);
+        res.write('<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"></head><body><h1>' +
+          decoded + 'が投稿されました</h1></body></html>');
         res.end();
       });
-      // res.end();
-      break;
-    case 'DELETE':
-      res.write('DELETE' + req.url);
       break;
     default:
       break;
   }
-  // res.end();
+
 }).on('error', (e) => {
-    console.error('[' + new Date() + '] Server Error', e);
+  console.error('[' + new Date() + '] Server Error', e);
 }).on('clientError', (e) => {
-    console.error('[' + new Date() + '] Client Error', e);
+  console.error('[' + new Date() + '] Client Error', e);
 });
-// const port = 8000;
-const port = process.env.PORT || 8000;
+const port = 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + ']Listening on ' + port);
+  console.info('[' + new Date() + '] Listening on ' + port);
 });
